1264fdb43487212c01484420fb8c5c0790509cc2
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-11-05 01:03:44 +0000

    Altered format to use optional parameter, instead of new function for forced calls

--- index.js
+++ index.js
@@ -22,40 +22,33 @@ module.exports = {
 	setApiKey: (newApiKey) => {
 		apiKey = newApiKey;
 	},
-	getSinglePlayer: (steamID) => {
+	getSinglePlayer: (steamID, forceUpdate = false) => {
 		if (isNaN(parseInt(steamID))){
 			return { isValid : false, content : "Please enter a valid steamID64" };
 		}
-		return getDataFromEndpoint(URL, `${endpoints.SINGLE_PLAYER}?${urlParams.API_KEY}=${apiKey}&${urlParams.STEAM_ID}=${steamID}`, { steamID : steamID });
+		const endpointUrl = formatForce(`${endpoints.SINGLE_PLAYER}?${urlParams.API_KEY}=${apiKey}&${urlParams.STEAM_ID}=${steamID}`, forceUpdate);
+		return getDataFromEndpoint(URL, endpointUrl, { steamID : steamID });
 	},
-	getSinglePlayerForce: (steamID) => {
+	getSinglePlayerMatches: (steamID, forceUpdate = false) => {
 		if (isNaN(parseInt(steamID))){
 			return { isValid : false, content : "Please enter a valid steamID64" };
 		}
-		return getDataFromEndpoint(URL, `${endpoints.SINGLE_PLAYER}?${urlParams.API_KEY}=${apiKey}&${urlParams.STEAM_ID}=${steamID}&${urlParams.FORCE_UPDATE}=true`, { steamID : steamID });
+		const endpointUrl = formatForce(`${endpoints.SINGLE_PLAYER_MATCHES}?${urlParams.API_KEY}=${apiKey}&${urlParams.STEAM_ID}=${steamID}`, forceUpdate);
+		return getDataFromEndpoint(URL, endpointUrl, { steamID : steamID });
 	},
-	getSinglePlayerMatches: (steamID) => {
-		if (isNaN(parseInt(steamID))){
-			return { isValid : false, content : "Please enter a valid steamID64" };
-		}
-		return getDataFromEndpoint(URL, `${endpoints.SINGLE_PLAYER_MATCHES}?${urlParams.API_KEY}=${apiKey}&${urlParams.STEAM_ID}=${steamID}`, { steamID : steamID });
+	getAllPlayers: (forceUpdate = false) =>{
+		const endpointUrl = formatForce(`${endpoints.ALL_PLAYERS}?${urlParams.API_KEY}=${apiKey}&${urlParams.OFFSET}=0`, forceUpdate);
+		return getDataFromEndpoint(URL, endpointUrl, null);
 	},
-	getSinglePlayerMatchesForce: (steamID) => {
-		if (isNaN(parseInt(steamID))){
-			return { isValid : false, content : "Please enter a valid steamID64" };
-		}
-		return getDataFromEndpoint(URL, `${endpoints.SINGLE_PLAYER_MATCHES}?${urlParams.API_KEY}=${apiKey}&${urlParams.STEAM_ID}=${steamID}&${urlParams.FORCE_UPDATE}=true`, { steamID : steamID });
-	},
-	getAllPlayers: () =>{
-		return getDataFromEndpoint(URL, `${endpoints.ALL_PLAYERS}?${urlParams.API_KEY}=${apiKey}&${urlParams.OFFSET}=0`, null);
-	},
-	getAllPlayersForce: () =>{
-		return getDataFromEndpoint(URL, `${endpoints.ALL_PLAYERS}?${urlParams.API_KEY}=${apiKey}&${urlParams.OFFSET}=0&${urlParams.FORCE_UPDATE}=true`, null);
-	},
-	getAllMatches: () =>{
-		return getDataFromEndpoint(URL, `${endpoints.ALL_MATCHES}?${urlParams.API_KEY}=${apiKey}&${urlParams.OFFSET}=0`, null);
-	},
-	getAllMatchesForce: () =>{
-		return getDataFromEndpoint(URL, `${endpoints.ALL_MATCHES}?${urlParams.API_KEY}=${apiKey}&${urlParams.OFFSET}=0&${urlParams.FORCE_UPDATE}=true`, null);
+	getAllMatches: (forceUpdate = false) =>{
+		const endpointUrl = formatForce(`${endpoints.ALL_MATCHES}?${urlParams.API_KEY}=${apiKey}&${urlParams.OFFSET}=0`, forceUpdate);
+		return getDataFromEndpoint(URL, endpointUrl, null);
 	}
+}
+
+function formatForce(endpointUrl, forceUpdate){
+	if (forceUpdate){
+		return endpointUrl += `&${urlParams.FORCE_UPDATE}=true`;
+	}
+	return endpointUrl;
 }
